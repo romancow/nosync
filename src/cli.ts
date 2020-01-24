0210326b65d4db5074645daefa38c08148776231
Sean Wilson <seancwilson@gmail.com>
2020-01-24 16:08:12 -0600

    add action implementation to cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,20 @@
-const { version } = require('../package.json')
+import version from 'consts:version'
 import program from 'commander'
-// import nosync from '../'
+import fs from 'fs-extra'
+import nosync from '.'
 
 program.version(version)
 	.option('-s, --settings <json file>', 'JSON settings file')
 	.option('-b, --base <path>', 'Base folder to store non-synced files')
 	.option('-o, --overwrite', 'Overwrite existing files in nosync folder')
-	.arguments('<paths...>')	//"Files or folders you don't want synced")
+	// .option('-c, --check', '')
+	.arguments('<paths...>')	//"Files or folders you don't want synced"
 	.action((paths: string[]) => {
-
+		// console.log(paths)
+		const settings = program.settings as string | undefined
+		const options = ((settings != null) && fs.existsSync(settings)) ?
+			fs.readJsonSync(settings) :
+			program.opts()
+		nosync(paths, options)
 	})
 	.parse(process.argv)

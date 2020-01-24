003ca3aced16bfd21365df2c22efdcc2ff212cb6
Sean Wilson <seancwilson@gmail.com>
2020-01-24 16:08:42 -0600

    make built cli executable

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import pkg from './package.json'
 import consts from 'rollup-plugin-consts'
+import executable from 'rollup-plugin-executable'
 
 export default [{
 	input: pkg.module,
@@ -11,13 +12,14 @@ export default [{
 	}
 }, {
 	input: 'es6/cli.js',
-	external: ['commander'],
+	external: ['commander', 'fs-extra', '.'],
 	output: {
 		file: pkg.bin,
 		format: 'cjs',
-		banner: '#! /usr/bin/env node'
+		banner: '#!/usr/bin/env node'
 	},
-	plugins: [consts({
-		version: pkg.version
-	})]
+	plugins: [
+		consts({ version: pkg.version }),
+		executable()
+	]
 }]

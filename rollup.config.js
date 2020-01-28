4a960dd43f3625a39c21311bf5497a983f6be4f1
Sean Wilson <seancwilson@gmail.com>
2020-01-28 17:46:38 -0600

    update rollup externals

--- rollup.config.js
+++ rollup.config.js
@@ -4,15 +4,14 @@ import executable from 'rollup-plugin-executable'
 
 export default [{
 	input: pkg.module,
-	external: ['fs-extra', 'path'],
-	// context: 'window',
+	external: ['fs-extra', 'os', 'path'],
 	output: {
 		file: pkg.main,
 		format: 'cjs',
 	}
 }, {
 	input: 'es6/cli.js',
-	external: ['commander', 'fs-extra', '.'],
+	external: ['commander', 'fs-extra', 'os', 'path', '.'],
 	context: 'global',
 	output: {
 		file: pkg.bin,

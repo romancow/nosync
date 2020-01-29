ffd63be4ab844e1f3cbba5aea055fead7e061c0e
Sean Wilson <seancwilson@gmail.com>
2020-01-29 10:06:10 -0600

    adds TSDoc

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,33 @@ import * as path from 'path'
 import * as utils from './utilities'
 import { PathMap, NoSyncOptions } from './options'
 
-function nosync(paths: string, options?: NoSyncOptions): void
+/**
+ * Moves the file or folder at the given `path` to a folder ignored by iCloud,
+ * and creates a symlink to it in its place.
+ *
+ * @param path - Path to the file or folder to not sync to iCloud
+ * @param options - Options for moving and symlinking file or folder
+ */
+function nosync(path: string, options?: NoSyncOptions): void
+
+/**
+ * Moves the files and folders at the given `paths` to a folder ignored by iCloud,
+ * and creates a symlink to them in their place.
+ *
+ * @param paths - Paths to the files or folders to not sync to iCloud
+ * @param options - Options for moving and symlinking files and folders
+ */
 function nosync(paths: string[], options?: NoSyncOptions): void
+
+/**
+ * Moves the files and folders at the given `paths` to a folder ignored by iCloud,
+ * and creates a symlink to them in their place.
+ *
+ * @param paths - An object mapping files or folders to be symlinked to where they should be located in the "nosync" folder
+ * @param options - Options for moving and symlinking files and folders
+ */
 function nosync(paths: PathMap, options?: NoSyncOptions): void
+
 function nosync(paths: string | string[] | PathMap, options: NoSyncOptions = {}) {
 	const { base = './.nosync', check = false, overwrite = false } = options
 	const normalizedPaths = PathMap.normalizePaths(paths)
--- src/options.ts
+++ src/options.ts
@@ -1,15 +1,37 @@
 import * as fs from 'fs-extra'
 import * as utils from './utilities'
 
+/**
+ * Options for moving and symlinking files and folders.
+ */
 export type NoSyncOptions = {
+	/**
+	 * The folder to use as the "nosync" folder where symlinked files and folder will be moved to
+	 */
 	base?: string
+	/**
+	 * Whether or not to ensure files or folders exist on an iCloud drive before "nosyncing" them
+	 */
 	check?: boolean
+	/**
+	 * Wether or not to overwrite existing files or folders in the "nosync" folder when moving them
+	 */
 	overwrite?: boolean
 }
 
+/**
+ * A map of files and folders to where they should symlinked to in the "nosync" folder.
+ */
 type PathMap = { [path: string]: string }
 
 namespace PathMap {
+
+	/**
+	 * Normalizes nosync paths (none, single, array, or map) to a `PathMap`.
+	 *
+	 * @param paths - The missing, single, array, or map of paths to normalize.
+	 * @returns A `PathMap` of nosync paths.
+	 */
 	export function normalizePaths(paths: undefined | null | string | string[] | PathMap): PathMap {
 		if (paths == null)
 			return {}
@@ -20,6 +42,12 @@ namespace PathMap {
 		else return paths
 	}
 
+	/**
+	 * Gets the nosync paths defined in a json file.
+	 *
+	 * @param file - A json file defining nosync paths
+	 * @returns A `PathMap` of nosync paths defined in the json `file`
+	 */
 	export function fromJson(file: string) {
 		const paths = fs.existsSync(file) ? fs.readJsonSync(file) : null
 		return paths ? normalizePaths(paths) : null
